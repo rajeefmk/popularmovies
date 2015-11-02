82d144a4041e3e5c4fcfeabab32c82b5cd780622
rmk <rajeefmk@gmail.com>
2015-11-02 18:46:39 +0530

    Cleanup project for submission

--- app/src/main/java/com/udacitynanodegree/rajeefmk/popularmovies/PopularMoviesApplication.java
+++ app/src/main/java/com/udacitynanodegree/rajeefmk/popularmovies/PopularMoviesApplication.java
@@ -6,11 +6,6 @@ import android.text.TextUtils;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
-import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.ImageScaleType;
-import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 
 public class PopularMoviesApplication extends Application {
     private RequestQueue mRequestQueue;
@@ -21,27 +16,7 @@ public class PopularMoviesApplication extends Application {
     public void onCreate() {
         super.onCreate();
         mInstance = this;
-        InitializeImageLoader();
 
-    }
-
-    private void InitializeImageLoader() {
-        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
-                getApplicationContext())
-                .defaultDisplayImageOptions(createDefaultOptions())
-                .memoryCache(new WeakMemoryCache())
-                .diskCacheSize(100 * 1024 * 1024).build();
-
-        com.nostra13.universalimageloader.core.ImageLoader.getInstance().init(config);
-    }
-
-    private DisplayImageOptions createDefaultOptions() {
-        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
-                .cacheInMemory(true).cacheOnDisk(true)
-                .imageScaleType(ImageScaleType.EXACTLY).resetViewBeforeLoading(true)
-                .displayer(new FadeInBitmapDisplayer(300)).build();
-
-        return defaultOptions;
     }
 
     public static synchronized PopularMoviesApplication getInstance() {
